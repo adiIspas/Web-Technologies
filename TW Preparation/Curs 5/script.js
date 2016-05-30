f1075e030b9a64147b67ebfac63a717da30156e0
adiIspas <adi.ispas95@gmail.com>
2016-05-30 23:34:21 +0300

    Finish Curs 5.

---
+++ TW Preparation/Curs 5/script.js
@@ -0,0 +1,14 @@
+function functia1(){
+  alert("Merge!");
+}
+
+function welcome(){
+  alert("Bine ai venit!");
+}
+
+function functia2(){
+  var x = document.getElementsByTagName("BODY")[0];
+    x.style.backgroundColor = "red";
+}
+
+window.onload = welcome;
